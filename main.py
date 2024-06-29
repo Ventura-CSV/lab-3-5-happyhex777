a913c2b94e03a75cd81549579fb0c37d3f41d3f9
Hector Diaz <hhectordiazz@icloud.com>
2024-06-28 18:32:02 -0700

    Complete 3-5  lab

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ def main():
     Code Your Program here
     ########################################
     """
-    duplication = 0
-
+    if num1 == num2 == num3:
+        duplication=3
+    elif num1==num2 or num1==num3 or num2==num3:
+        duplication=2
+    else:
+        duplication = 0
+    print("duplication",duplication)
     ########################################
     # Do not delete the return statement
     ########################################
     return duplication
-
 
 if __name__ == '__main__':
     main()
